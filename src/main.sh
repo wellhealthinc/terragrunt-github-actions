77d7f0ce20c3fc3060b782b80e9dcedb88bc20c6
welljonathan <jhuizingh@wellapp.com>
2021-08-27 09:21:08 -0700

    More debug info

--- src/main.sh
+++ src/main.sh
@@ -108,7 +108,7 @@ function installTerraform {
   echo "Successfully downloaded Terraform v${tfVersion}"
 
   echo "Unzipping Terraform v${tfVersion}"
-  unzip -d /usr/local/bin /tmp/terraform_${tfVersion} &> /dev/null
+  unzip -d /usr/local/bin /tmp/terraform_${tfVersion} #&> /dev/null
   if [ "${?}" -ne 0 ]; then
     echo "Failed to unzip Terraform v${tfVersion}"
     exit 1
